96d0301841b3dcb9eb4d825d0209c223ac2e11db
RutgerSmeenk <144896389+RutgerSmeenk@users.noreply.github.com>
2023-09-14 01:43:22 +0200

    Add files via upload

---
+++ Exp 7 - one dim Core/exp7a/CP_ALS_Core_predict.m
@@ -0,0 +1,42 @@
+function [pred, var_CI, var_PI] = CP_ALS_Core_predict(XTest, W, norms, M, sigma, sto_core, Y, X)
+    [N_test,~] = size(XTest);
+    [N,~] = size(X);
+    D = length(1:log2(M)) ;
+    YTest = ones(N_test,1);
+    pred = ones(N_test,1);
+    pred_train = ones(N,1);
+    for m = 1:D
+        feat{m} = features(XTest,m, M) ;
+        pred = pred.*(feat{m} * W{m}) ;
+        pred_train = pred_train.*(features(X, m, M) * W{m}) ;
+    end
+
+        m = sto_core ;
+        ZW_train = pred_train./(features(X,m, M) * W{m});
+        ZW_test = pred./(features(XTest,m, M) * W{m});
+        [CC_train, ~, ~] = C_matrices(features(X,m, M) , ZW_train, Y) ;
+        [~, ~, c_test] = C_matrices(feat{m}, ZW_test, YTest) ;
+        %C_test = c_test ;
+        %C_train = c_train ;
+        %regu = reg./(W{d}'*W{d}) ;
+
+    pred = pred * norms ;
+    pred_train = pred_train*norms ;
+    
+    sigmae = var(Y - pred_train, 1) ;
+    
+
+    % d = sto_core ;
+    % Z_test = feat{d} ;
+    % ZW_test = ZW./(feat{d}*W_mean{d}); 
+    % [~,~, c_test] = C_matrices(Z_test,ZW_test, YTest) ;
+
+    num = numel(W{m}) ;
+    P0 = sigma*eye(num) ;
+    P0_inv = inv(P0) ;
+    W_cov = pinv(CC_train./sigmae + P0_inv) ;
+
+    var_CI = diag(c_test * W_cov * c_test')  ;
+
+    var_PI = var_CI + sigmae ;
+end
---
+++ Exp 7 - one dim Core/exp7a/CP_ALS_weights.m
@@ -0,0 +1,43 @@
+function [W, norms, reg] = CP_ALS_weights(X, y, M, R, lambda, maxIte)
+
+    D = length(1:log2(M)) ;
+    N = size(X,1) ;
+    W = cell(1,D);
+    ZW = 1;
+    reg = 1;
+    norms = ones(R, 1) ;
+
+    % Initialization
+    for m = D:-1:1
+        W{m} = randn(2,R);
+        W{m} = W{m}/norm(W{m});
+        reg = reg.*(W{m}'*W{m});
+        Z = features(X,m,M);
+        ZW = (Z*W{m}).*ZW;
+    end
+    
+    % Main loop
+    itemax = maxIte*(2*(D-1))+1;
+    for ite = 1:itemax
+        loopind = mod(ite-1,2*(D-1))+1;
+        if loopind <= D
+            m = loopind;
+        else
+            m = 2*D-loopind;
+        end
+        Z = features(X,m,M);
+        reg = reg./(W{m}'*W{m});
+        ZW = ZW./(Z*W{m}); 
+        [CC,Cy,~] = C_matrices(Z,ZW,y);
+        x = (CC+lambda*N*kron(reg,eye(2)))\Cy;
+        clear CC Cy
+        W{m} = reshape(x,2,R);
+        reg = reg.*(W{m}'*W{m});
+        ZW = ZW.*(Z*W{m});
+        % Normalization
+        [W, norms] = normalization_cpd(W,norms) ;
+    end
+end
+
+
+
