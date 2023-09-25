de1381e60a6216f8fc5c25bbedf9921db6259f73
RutgerSmeenk <144896389+RutgerSmeenk@users.noreply.github.com>
2023-09-25 15:26:29 +0200

    Add files via upload

---
+++ Exp 2 - two dim freq/normalization_cpd.m
@@ -0,0 +1,24 @@
+function [W_new, norms_new] = normalization_cpd(W,norms)
+
+D = size(W,2) ;
+R = size(W{1},2) ;
+norms_new = norms ;
+
+for d = 1:D
+    for r = 1:R
+        
+        W_r = W{d}(:,r) ;
+        normm = norm(W_r) ;
+
+        W_new{d}(:,r) = W_r / normm ;
+        norms_new(r) = norms_new(r) * normm ;
+
+    end
+end
+
+end
+
+
+
+
+
