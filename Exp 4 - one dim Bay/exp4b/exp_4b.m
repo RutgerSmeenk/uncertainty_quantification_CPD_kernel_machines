5b945602ea579e5ef6ff2a4a1f07c14750b647e9
RutgerSmeenk <144896389+RutgerSmeenk@users.noreply.github.com>
2023-09-25 15:48:20 +0200

    Add files via upload

---
+++ Exp 4 - one dim Bay/exp4b/exp_4b.m
@@ -0,0 +1,90 @@
+% This study regards a dataset which is learned and where predictions are
+% made and the uncertainty in the predictions is quantified 
+%clear all
+rng(20) ;
+M = 8;
+R = 20;
+maxIte = 10;
+
+warning('off','all');
+
+load("X.mat","XTest.mat","Y.mat","YTest.mat")
+
+%% K fold cross validation
+[mean_error_all_Bay, conf_error_all_Bay, sigma_folds_Bay, noise_folds_Bay] = cross_Bay_dat(X, Y, M, R, maxIte) ;
+
+% Find the minimum element in the matrix
+minValue = min(mean_error_all_Bay);
+
+% Calculate the threshold for the difference
+threshold = 0.5 * minValue;
+
+% Find the indices of the minimum elements that meet the condition
+index_conf = find(mean_error_all_Bay > minValue + threshold);
+
+conf_error_all_Bay(index_conf) = 10e20 ;
+
+matrix_conf = reshape(conf_error_all_Bay,[length(sigma_folds_Bay), length(noise_folds_Bay)]) ;
+
+min_value = min(matrix_conf(:)) ;
+
+% Find the indices of the minimum value using find
+[index_sigma, index_sigmae] = find(matrix_conf == min_value);
+
+% Extract elements
+sigma_Bay = sigma_folds_Bay(index_sigma);
+sigmae_Bay = noise_folds_Bay(index_sigmae);
+
+%% Making predictions
+
+% Obtaining the weights
+[W_mean, W_cov, norms] = CP_ALS_Bay_weights(X, Y, M, R, maxIte, sigma_Bay, sigmae_Bay) ;
+
+% Prediction
+[pred_Bay, var_CI, var_PI] = CP_ALS_Bay_predict(XTest, W_mean, W_cov, norms, M, sigmae_Bay) ;
+
+pred_Bay = real(pred_Bay) ;
+var_CI = real(var_CI) ;
+var_PI = real(var_PI) ;
+
+% Uncertainty intervals
+CI_lower_Bay = pred_Bay - (2*sqrt(var_CI)) ;
+CI_upper_Bay = pred_Bay + (2*sqrt(var_CI)) ;
+
+%[mUT, PUT] = uns(W_mean, W_cov, norms) ;
+
+% 
+% variableNames = {'CI_lower_Bay', 'CI_upper_Bay','pred_Bay'};
+% 
+% for i = 1:length(variableNames)
+%     filePath1 = fullfile('C:\Users\rutge\Documents\MATLAB\Thesis\Exp 1 - one dim Freq\exp_1b', [variableNames{i}, '.mat']);
+%     save(filePath1, variableNames{i});
+% end
+
+%% Plotting
+
+a = [XTest, YTest, CI_lower_Bay, CI_upper_Bay, pred_Bay] ; 
+
+Plot = sortrows(a);
+
+true_model = [Plot(:,1),Plot(:,3)];
+pred_train = Plot(:,2);
+
+
+figure(2)
+
+plot(Plot(:,1),Plot(:,2),'ko');
+hold on
+plot(Plot(:,1),Plot(:,5),'b', 'LineWidth', 2);
+plot(Plot(:,1),Plot(:,3),'r--', 'LineWidth', 2)
+plot(Plot(:,1),Plot(:,4),'r--', 'LineWidth', 2);
+
+
+%l(1).MarkerSize = 2; 
+%l(2).MarkerSize = 12;
+%title('Bayesian','interpreter','latex')
+xlabel('Feature','interpreter','latex')
+ylabel('Function','interpreter','latex')
+%ylim([-2 2])
+%xlim([-100 100])
+legend('Test input','Prediction','Bayesian $95\%$ confidence interval','','Location','northwest','interpreter','latex','FontSize', 12)
